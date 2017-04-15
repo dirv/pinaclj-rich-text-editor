a2ca176d01c3c1227ae69b439a50173a8566ca47
Daniel Irvine <daniel@danielirvine.com>
2017-04-15 17:16:00 +0100

    Flesh out rendering of Hiccup nodes

--- src/cljs/pinaclj_rich_text_editor/render.cljs
+++ src/cljs/pinaclj_rich_text_editor/render.cljs
@@ -8,14 +8,24 @@
       :parent-element)
     :text-node))
 
+(defn- ->attr-stream [attrs]
+  (mapcat #(list (name (key %)) (val %)) attrs))
+
 (defmulti render-element select-type)
+
+(defn- render-parent [tag attrs children]
+  (apply idom/elementOpen (name tag) nil [] (->attr-stream attrs))
+  (doall (map render-element children))
+  (idom/elementClose (name tag)))
+
 (defmethod render-element :leaf-element [[tag]]
   (idom/elementVoid (name tag) nil []))
 
-(defmethod render-element :parent-element [[tag child]]
-  (idom/elementOpen (name tag))
-  (render-element child)
-  (idom/elementClose (name tag)))
+(defmethod render-element :parent-element
+  ([[tag & [attrs-or-first-child & remaining-children :as all-children]]]
+   (if (map? attrs-or-first-child)
+     (render-parent tag attrs-or-first-child remaining-children)
+     (render-parent tag {} all-children))))
 
 (defmethod render-element :text-node [text]
   (idom/text text))
--- test/cljs/pinaclj_rich_text_editor/render_test.cljs
+++ test/cljs/pinaclj_rich_text_editor/render_test.cljs
@@ -14,4 +14,8 @@
   (testing "a character in a paragraph"
     (is (= "<p>C</p>" (render [:p "C"]))))
   (testing "a tree of nodes"
-    (is (= "<p><b>C</b></p>" (render [:p [:b "C"]])))))
+    (is (= "<p><b>C</b></p>" (render [:p [:b "C"]]))))
+  (testing "a parent element with attribute"
+    (is (= "<p class=\"test\"></p>" (render [:p {:class "test"} ""]))))
+  (testing "mutliple children"
+    (is (= "<p><b>C</b><i>D</i></p>" (render [:p [:b "C"] [:i "D"]])))))
