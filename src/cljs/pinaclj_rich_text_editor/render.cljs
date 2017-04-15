d07688d44e65c567560c16484ee130193c49351c
Daniel Irvine <daniel@danielirvine.com>
2017-04-15 16:43:34 +0100

    Refactor render to use a multimethod

--- src/cljs/pinaclj_rich_text_editor/render.cljs
+++ src/cljs/pinaclj_rich_text_editor/render.cljs
@@ -1,15 +1,24 @@
 (ns pinaclj-rich-text-editor.render
   (:require [incremental-dom :as idom]))
 
-(defn- render-element [[tag content]]
+(defn- select-type [node]
+  (if (vector? node)
+    (if (= 1 (count node))
+      :leaf-element
+      :parent-element)
+    :text-node))
+
+(defmulti render-element select-type)
+(defmethod render-element :leaf-element [[tag]]
+  (idom/elementVoid (name tag) nil []))
+
+(defmethod render-element :parent-element [[tag child]]
   (idom/elementOpen (name tag))
-  (cond
-    (string? content)
-    (idom/text content)
-    (vector? content) (render-element content))
+  (render-element child)
   (idom/elementClose (name tag)))
+
+(defmethod render-element :text-node [text]
+  (idom/text text))
 
 (defn render [doc node]
   (idom/patch node #(render-element %) doc))
-
-
