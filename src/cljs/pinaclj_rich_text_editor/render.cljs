cf856859ee6c15d616b9840b5246b246881dcb39
Daniel Irvine <daniel@danielirvine.com>
2017-04-16 22:39:51 +0100

    Pass keys to incremental dom

--- src/cljs/pinaclj_rich_text_editor/render.cljs
+++ src/cljs/pinaclj_rich_text_editor/render.cljs
@@ -1,9 +1,11 @@
 (ns pinaclj-rich-text-editor.render
-  (:require [incremental-dom :as idom]))
+  (:require [incremental-dom :as idom]
+            [pinaclj-rich-text-editor.hiccup :as hiccup]))
+
 
 (defn- select-type [node]
   (if (vector? node)
-    (if (= 1 (count node))
+    (if (or (= 1 (count node)) (and (= 2 (count node)) (map? (second node))))
       :leaf-element
       :parent-element)
     :text-node))
@@ -14,12 +16,12 @@
 (defmulti render-element select-type)
 
 (defn- render-parent [tag attrs children]
-  (apply idom/elementOpen (name tag) nil [] (->attr-stream attrs))
+  (apply idom/elementOpen (name tag) (get attrs :key) [] (->attr-stream attrs))
   (doall (map render-element children))
   (idom/elementClose (name tag)))
 
-(defmethod render-element :leaf-element [[tag]]
-  (idom/elementVoid (name tag) nil []))
+(defmethod render-element :leaf-element [[tag attrs]]
+  (idom/elementVoid (name tag) (get attrs :key) []))
 
 (defmethod render-element :parent-element
   ([[tag & [attrs-or-first-child & remaining-children :as all-children]]]
@@ -29,9 +31,6 @@
 
 (defmethod render-element :text-node [text]
   (idom/text text))
-
-(defn render [doc node]
-  (idom/patch node #(render-element %) doc))
 
 (defn render-all [children node]
   (idom/patch node #(doall (map render-element children))))
--- test/cljs/pinaclj_rich_text_editor/render_test.cljs
+++ test/cljs/pinaclj_rich_text_editor/render_test.cljs
@@ -1,6 +1,7 @@
 (ns pinaclj-rich-text-editor.render-test
   (:require-macros [cljs.test :refer [deftest testing is async]])
   (:require [cljs.test :as test]
+            [incremental-dom :as idom]
             [pinaclj-rich-text-editor.render :as render]))
 
 (defn- render [node]
@@ -19,3 +20,36 @@
     (is (= "<p class=\"test\"></p>" (render [:p {:class "test"} ""]))))
   (testing "mutliple children"
     (is (= "<p><b>C</b><i>D</i></p>" (render [:p [:b "C"] [:i "D"]])))))
+
+(deftest idom-methods []
+  (testing "uses idom/elementVoid for leaf elements"
+    (let [calls (atom [])]
+      (with-redefs [idom/elementVoid (fn [& args] (swap! calls conj args))]
+        (render [:p])
+        (is (= 1 (count @calls)))
+        (is (= ["p" nil []] (first @calls))))))
+  (testing "uses idom/text for text nodes"
+    (let [calls (atom [])]
+      (with-redefs [idom/text (fn [& args] (swap! calls conj args))]
+        (render "text")
+        (is (= 1 (count @calls)))
+        (is (= ["text"] (first @calls))))))
+  (testing "uses idom/elementVoid for parents with attrs"
+    (let [calls (atom [])]
+      (with-redefs [idom/elementVoid (fn [& args] (swap! calls conj args))]
+        (render [:p {:test "1"}])
+        (is (= 1 (count @calls)))
+        (is (= ["p" nil []]) (first @calls))))))
+
+(deftest element-keys []
+  (testing "passes key to parent elements"
+    (let [given-key (atom nil)]
+      (with-redefs [idom/elementOpen #(reset! given-key %2)
+                idom/elementClose (fn [& _] )]
+        (render [:p {:key "A"} [:b]])
+        (is (= "A" @given-key)))))
+  (testing "passes key for leaf elements"
+    (let [given-key (atom nil)]
+      (with-redefs [idom/elementVoid #(reset! given-key %2)]
+        (render [:p {:key "A"}])
+        (is (= "A" @given-key))))))
